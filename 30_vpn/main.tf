f247ce5b43b3f7a5b195649b401b1ff35db96603
mohammadsyed397 <mohammad.syed601@gmail.com>
2025-07-11 17:52:50 -0400

    latest

--- 30_vpn/main.tf
+++ 30_vpn/main.tf
@@ -5,7 +5,7 @@ resource "aws_instance" "bastion" {
     subnet_id = local.public_subnet_id
     instance_type = "t3.micro"
     key_name = "king"
-    user_data = file("open-vpn.ssh")
+    user_data = file("openvpn.ssh")
     tags = merge (
         local.common_tags,{
             Name =  "$(var.project)-$(var.environment)-bastion"
