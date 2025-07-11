165d31a54a3a52604114b2a4f9b2f8de89069d47
mohammadsyed397 <mohammad.syed601@gmail.com>
2025-07-10 21:56:25 -0400

    latest

--- 10_sg/main.tf
+++ 10_sg/main.tf
@@ -1,20 +1,46 @@
+module "backend_alb" {
+    source = "git::https://github.com/mohammadsyed397/terraform-aws-securitygroup.git?ref=main"
+    project = var.project
+    environment = var.environment
+
+    sg_name = "backend-alb"
+    sg_description = "for backend alb"
+    vpc_id = local.vpc_id
+}
+
 module "frontend" {
     source = "git::https://github.com/mohammadsyed397/terraform-aws-securitygroup.git?ref=main"
-    sg_description = var.sg_description
-    sg_name = var.sg_name
     project = var.project
     environment = var.environment
-    vpc_id = local.vpc_id
 
-  
+    sg_name = "frontend-securitygroup"
+    sg_description = "This security group is for frontend"
+    vpc_id = local.vpc_id
 }
 module "bastion" {
+    #source = "../../terraform-aws-securitygroup"
     source = "git::https://github.com/mohammadsyed397/terraform-aws-securitygroup.git?ref=main"
-    sg_description = var.bastion_sg_description
-    sg_name = var.bastion_sg_name
     project = var.project
     environment = var.environment
-    vpc_id = local.vpc_id
 
-  
+    sg_name = var.bastion_sg_name
+    sg_description = var.bastion_sg_description
+    vpc_id = local.vpc_id
+}
+
+resource "aws_security_group_rule" "backend_alb_bastion" {
+  type              = "ingress"
+  from_port         = 80
+  to_port           = 80
+  protocol          = "tcp"
+  source_security_group_id = module.bastion.sg_id
+  security_group_id = module.backend_alb.sg_id
+}
+resource "aws_security_group_rule" "bastion_laptop" {
+  type              = "ingress"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = module.bastion.sg_id
 }
--- 20_bastion/main.tf
+++ 20_bastion/main.tf
@@ -1,6 +1,14 @@
 resource "aws_instance" "bastion" {
     ami = local.ami_id
-    vpc_security_group_ids = local.bastion_sg_id
+    vpc_security_group_ids = [local.bastion_sg_id]
     subnet_id = local.public_subnet_id
     instance_type = "t3.micro"
+    tags = merge (
+        local.common_tags,{
+            Name =  "$(var.var.project)-$(var.var.environment)-bastion"
+
+        }
+        
+        
+    )
 }
