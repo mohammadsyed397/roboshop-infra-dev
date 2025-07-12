6d0917918d274a9bd1573fc3c7c7c9f7e61cd53f
mohammadsyed397 <mohammad.syed601@gmail.com>
2025-07-12 12:07:38 -0400

    latest

--- 60-catalogue/provider.tf
+++ 60-catalogue/provider.tf
@@ -18,23 +18,3 @@ provider "aws" {
     region = "us-east-1"
   
 }
-terraform {
-  required_providers {
-    aws = {
-
-    source = "hashicorp/aws"
-    version = "5.98.0"
-    }
-  }
-  backend "s3" {
-    bucket        = "terraform-roboshop-prac"
-    key           = "roboshop-infra-dev-catalogue"
-    region        = "us-east-1"
-    encrypt       = true
-
-  }
-}
-provider "aws" {
-    region = "us-east-1"
-  
-}
