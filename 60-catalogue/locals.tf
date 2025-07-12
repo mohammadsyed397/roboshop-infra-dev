3045071945e5088eed93134f94c0a5440d41ed13
mohammadsyed397 <mohammad.syed601@gmail.com>
2025-07-12 12:11:28 -0400

    latest

--- 60-catalogue/locals.tf
+++ 60-catalogue/locals.tf
@@ -1,7 +1,7 @@
 locals {
     vpc_id = data.aws_ssm_parameter.vpc_id.value
     private_subnet_ids = split ("," , data.aws_ssm_parameter.private_subnet_ids.value)[0]
-    catalogue_sg_id = data.aws_ssm_parameter.catalogue_sg_id
+    catalogue_sg_id = [data.aws_ssm_parameter.catalogue_sg_id]
     ami_id = data.aws_ami.joindevops.id
     
 
--- 60-catalogue/main.tf
+++ 60-catalogue/main.tf
@@ -17,7 +17,7 @@ resource "aws_lb_target_group" "catalogue" {
 resource "aws_instance" "catalogue" {
     ami = local.ami_id
     instance_type = "t3.micro"
-    vpc_security_group_ids = local.catalogue_sg_id
+    vpc_security_group_ids = [local.catalogue_sg_id]
     subnet_id = local.private_subnet_ids
     tags = merge (
         local.common_tags,{
